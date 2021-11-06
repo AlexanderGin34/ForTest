f0960801a678dea33e0f9ec255de5d56c84f00d8
AlexGin <Meinepostkam@gmail.com>
2021-11-07 01:43:15 +0300

    Initial Commit

---
+++ ForTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForTest
+//
+//  Created by Aleksandr Gineyka on 07.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
